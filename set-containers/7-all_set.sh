984c06da4880c1ac1256f4888dd87b6bb38f01a3
Prismoid <seike.hirotsugu@gmail.com>
2025-04-28 15:57:30 +0900

    Update 7-all_set.sh

--- set-containers/7-all_set.sh
+++ set-containers/7-all_set.sh
@@ -2,20 +2,20 @@
 set -e
 set -o pipefail
 
-cd ${WORKDIR}/data-share-handson-scripts/set-containers
+cd ${WORKDIR}/cadde-data-share-scripts/set-containers
 bash 1-ckan_set.sh
 
-cd ${WORKDIR}/data-share-handson-scripts/set-containers
+cd ${WORKDIR}/cadde-data-share-scripts/set-containers
 bash 2-prov_set.sh
 
-cd ${WORKDIR}/data-share-handson-scripts/set-containers
+cd ${WORKDIR}/cadde-data-share-scripts/set-containers
 bash 3-authz_set.sh
 
-cd ${WORKDIR}/data-share-handson-scripts/set-containers
+cd ${WORKDIR}/cadde-data-share-scripts/set-containers
 bash 4-cons_set.sh
 
-cd ${WORKDIR}/data-share-handson-scripts/set-containers
+cd ${WORKDIR}/cadde-data-share-scripts/set-containers
 bash 5-webapp_set.sh
 
-cd ${WORKDIR}/data-share-handson-scripts/set-containers
+cd ${WORKDIR}/cadde-data-share-scripts/set-containers
 bash 6-http-server_set.sh
